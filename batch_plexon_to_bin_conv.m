a0d9bc0088174198ac7176717a35d45f589609c7
o-zhu <zhuou@uchicago.edu>
2020-02-16 13:26:56 -0600

    Batch plexon to binary file converter

---
+++ batch_plexon_to_bin_conv.m
@@ -0,0 +1,37 @@
+%% Batch plx/pl2 to binary converter
+
+% default options are in parenthesis after the comment
+tic; % start timer
+PATH = 'D:\\Kilosort2PLX\\'; % edit this to be the path to your Kilosort folder
+dataPATH = strcat(PATH, 'RawData\\'); % path to your raw data folder
+conv_dataPATH = strcat(PATH, 'ConvertedData\\');
+rawdatafolder = dir(dataPATH);
+FILE = 0;
+% p = gcp();
+% errorpath = strcat(PATH, 'ErrorFiles\\'); % path to error files folder,where files will go if they cause errors(un-comment try/catch statements at lines 9, 41-44) 
+for i=1:length(rawdatafolder)
+    if getfield(rawdatafolder(i),'isdir') ~= 1
+%          try                                % un-comment if some files are causing errors, keep commented if all/most files are causing errors
+            PLXnameAlone = getfield(rawdatafolder(i),'name');
+            fprintf('Current file is: ')
+            fprintf(PLXnameAlone)
+
+            %PLXnameAlone = input('Please enter the name of the PLEXON file to be sorted (in single quotes with suffix): ');
+            [ops] = PLXStandardConfig(PLXnameAlone,PATH,dataPATH);
+
+            fprintf('Time %3.0fs. ', toc)
+            %
+            if ops.GPU     
+                fprintf('Initializing GPU ...\n')
+                gpuDevice(1); % initialize GPU (will erase any existing GPU arrays)
+            end
+
+            [uproj, ops] = PLXpreprocessData(ops); % preprocess data and extract spikes for initialization
+            
+            movefile(strcat(dataPATH,PLXnameAlone), conv_dataPATH)
+    end
+    disp(strcat('Converted: ', PLXnameAlone))
+    FILE = FILE +1;
+end
+
+disp(strcat('Files converted: ', num2str(FILE)))
